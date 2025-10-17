36c8c620af3f1436036478141b4224f97f0579bd
wclausen-square <wclausen@squareup.com>
2025-10-17 12:12:17 -0700

    Add initial gradle project structure (#6)
    Includes the following gradle projects:
    - `:cli`: makes code in `:core` available/executable from the command
    line
    - `:core`: main logic for artifact sync (e.g. downloading artifacts,
    hashing modules, etc)
    - `:gradle-plugin`: the gradle plugin to apply to client projects for
    integration with artifact sync

---
+++ cli/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+
+    // Apply the Application plugin to add support for building an executable JVM application.
+    application
+}
+
+group = "xyz.block.artifactswap"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+application {
+    mainClass = "xyz.block.artifactswap.cli.MainKt"
+}
---
+++ core/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+}
+
+group = "xyz.block.artifactswap"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
