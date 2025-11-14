626995f6f743178712a61cb1089e2e29069ca37c
Josh Friend <jfriend@block.xyz>
2025-11-14 13:51:27 -0500

    Fix analytics endpoint path (#52)
    Too much replacement with generic values
    
    
    https://github.com/block/ide-metrics-plugin/blob/2c598840afa749cfc65b6e47fb3b48db387b12c3/kotlin-eventstream2/client/src/main/kotlin/com/squareup/eventstream/EventstreamService.kt#L13

--- core/src/main/kotlin/xyz/block/artifactswap/core/eventstream/EventstreamService.kt
+++ core/src/main/kotlin/xyz/block/artifactswap/core/eventstream/EventstreamService.kt
@@ -4,11 +4,9 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
-import xyz.block.artifactswap.core.config.EVENTSTREAM_GZIP_HEADER
-import xyz.block.artifactswap.core.config.EVENTSTREAM_LOG_EVENTS_PATH
 
 public interface EventstreamService {
-  @Headers(EVENTSTREAM_GZIP_HEADER)
-  @POST(EVENTSTREAM_LOG_EVENTS_PATH)
+  @Headers("X-Square-Gzip: true")
+  @POST("/2.0/log/eventstream")
   public fun logEvents(@Body request: LogEventStreamV2Request): Call<LogEventStreamV2Response>
 }
