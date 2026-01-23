50a6f5cedce5e39cf6626384c8f8887d651c0f2d
Josh Friend <jfriend@block.xyz>
2026-01-23 16:46:05 -0500

    Add ide-plugin project with basic setup for publish/verification

---
+++ ide-plugin/build.gradle.kts
@@ -0,0 +1,93 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+  id("org.jetbrains.intellij.platform")
+}
+
+repositories {
+  mavenCentral()
+  intellijPlatform {
+    defaultRepositories()
+  }
+}
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(21))
+  }
+}
+
+// Otter 3 stable
+val androidStudioTarget = "2025.2.3.9"
+
+dependencies {
+  intellijPlatform {
+    // Use Android Studio as base IDE to get Android plugin bundled
+    androidStudio(androidStudioTarget)
+
+    bundledPlugin("com.intellij.gradle")
+    bundledPlugin("com.intellij.java")
+    bundledPlugin("org.jetbrains.kotlin")
+    
+    // Android plugin is bundled with Android Studio
+    bundledPlugin("org.jetbrains.android")
+
+    // Artifact Swap builds on top of Spotlight
+    plugin("com.fueledbycaffeine.spotlight:${libs.versions.spotlight.get()}")
+
+    intellijPlatformTesting.runIde.register("runLocalIde") {
+      // https://plugins.jetbrains.com/docs/intellij/android-studio.html#configuring-the-plugin-gradle-build-script
+      val path = providers.gradleProperty("intellijPlatformTesting.idePath").orNull
+      if (path != null) {
+        localPath.set(file(path))
+      }
+    }
+  }
+}
+
+val projectVersion: String = providers.gradleProperty("version").get()
+
+fun isSnapshot(): Boolean = projectVersion.endsWith("SNAPSHOT")
+
+version = if (isSnapshot()) {
+  "$projectVersion-${LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))}"
+} else {
+  projectVersion
+}
+
+// https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
+intellijPlatform {
+  pluginConfiguration {
+    name = "Artifact Swap"
+    description = """
+      Manage large Gradle builds in your IDE by swapping projects with prebuild artifacts (https://engineering.block.xyz/blog/shrinking-elephants)
+    """.trimIndent()
+
+    ideaVersion {
+      // Android studio Otter 1 equivalent
+      // https://plugins.jetbrains.com/docs/intellij/android-studio-releases-list.html
+      sinceBuild = "251"
+    }
+  }
+
+  publishing {
+    token = providers.environmentVariable("JETBRAINS_MARKETPLACE_SQUARE_PLUGINS")
+      .orElse(providers.gradleProperty("jetbrainsMarketplaceToken"))
+    channels = if (isSnapshot()) listOf("EAP") else listOf("Stable")
+  }
+
+  pluginVerification {
+    ides {
+      create(IntelliJPlatformType.AndroidStudio, androidStudioTarget) {}
+      // Also verify against IntelliJ IDEA (where Android plugin won't be available)
+      create(IntelliJPlatformType.IntellijIdea, "2025.3.1") {}
+    }
+
+    // Unresolved Android plugin classes are expected when verifying against IntelliJ IDEA
+    // These are from the optional Android plugin dependency and are handled at runtime
+    // See ignored-problems.txt for the list of ignored patterns
+  }
+}
